fc85f80f8493654a79bf147d078ac836de11dcee
Vinicius Rosa <viniciushcrosa@gmail.com>
2021-05-11 21:07:55 -0300

    semana 3

---
+++ semana_4/4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+double areaTrapezio(double baseMaior, double baseMenor, double altura)
+{
+  return (baseMaior + baseMenor) * altura / 2;
+}
+
+double areaRetangulo(double base, double altura)
+{
+  double area = areaTrapezio(base, base, altura);
+  printf("Area do retangulo: %f\n", area);
+  return area;
+}
+
+double areaDoQuadrado(double lado)
+{
+  return areaRetangulo(lado, lado);
+}
+
+int main()
+{
+  double retangulo = areaRetangulo(11, 27);
+  printf(" %f \n", retangulo);
+  double quadrado = areaDoQuadrado(10.0);
+  printf(" %f \n", quadrado);
+  return 0;
+}
